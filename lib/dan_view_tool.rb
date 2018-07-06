a4d6fec65f38529075dc047230555bd7f4dc3604
Dan Uluma <danuluma@gmail.com>
2018-07-06 19:54:55 +0300

    my view toolinitial commit

---
+++ lib/dan_view_tool.rb
@@ -0,0 +1,6 @@
+require "dan_view_tool/version"
+require "dan_view_tool/renderer"
+
+module DanViewTool
+  # Your code goes here...
+end
---
+++ lib/dan_view_tool/version.rb
@@ -0,0 +1,3 @@
+module DanViewTool
+  VERSION = "0.1.0"
+end
